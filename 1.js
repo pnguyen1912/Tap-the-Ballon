d985c73578734632e0edad16de4ff3add901170f
Patton Nguyen <41391357+pnguyen1912@users.noreply.github.com>
2018-11-30 22:22:00 -0600

    Add files via upload

--- 1.js
+++ 1.js
@@ -23,8 +23,13 @@ class monkeyPage {
         var y = setInterval(function () {
             hidebtn1.style.display = 'none'
         }, 1);
+        
         var a = setInterval(function () {
             let dot = document.getElementById('dot1');
+            let sW = screen.height - dot.height;
+            let sH = screen.width - dot.width;
+            dot.style.top = Math.round(Math.random* sW) +'px';
+            dot.style.left = Math.round(Math.random*sH) +'px';
             if (dot.style.display == 'none') {
                 dot.style.display = 'block';
             } else {
@@ -67,10 +72,12 @@ class monkeyPage {
         var d = setInterval(function () {
 
             let create = document.getElementById('result');
-            create.innerHTML = `Your Score ${count}`
+            create.innerHTML = `YOUR SCORE ${count} <br>
+            HIGH SCORE: ${max}`;
             create.style.display = 'block';
             hidebtn.style.display = 'block';
             hidebtn1.style.display = 'block';
+            
             if (count > 0) {
                 count = 0
             }
@@ -78,8 +85,6 @@ class monkeyPage {
         setTimeout(function () {
             clearInterval(d);
         }, 5001);
-
-
         var e = setInterval(function () {
             let i = document.getElementById('newgame');
             i.style.display = 'block';
@@ -88,25 +93,35 @@ class monkeyPage {
             clearInterval(e);
         }, 5001);
     }
+    // pop 2nd page
     clickyes() {
         let yes = document.getElementById('yes');
         yes.style.display = 'none';
         let page2 = document.getElementById('page2');
         page2.style.display = 'block';
     }
-
+    // pop 3rd page
     clicksubmit() {
         let submit = document.getElementById('page3');
         submit.style.display = 'block';
         let page2 = document.getElementById('page2');
         page2.style.display = 'none';
+        let create1 = document.getElementById('point');
+            create1.innerHTML = `+${max}00 ⭐`
+
     }
 }
 let myPage = new monkeyPage();
+let max =0;
 
+// Keep and display the score
 let count = 0;
-
 function upcount() {
     count++;
+    if (count >max){
+        max=count
+    }
+    sound = document.getElementById("sound");
+    sound.play();
     console.log(count)
 }
